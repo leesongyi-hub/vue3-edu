5eff2ee26ba12d17b83fdda1439403d9df818fc6
leesongyi-hub <okpo5071@gmail.com>
2023-02-11 16:12:58 +0900

    가위바보실습

--- vue3-lsy-lotto/src/components/LottoHeader.vue
+++ vue3-lsy-lotto/src/components/LottoHeader.vue
@@ -19,17 +19,13 @@
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">로또</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">가위바위보</a>
+            </li>
           </ul>
           <form class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-success" type="submit">Search</button>
           </form>
         </div>
       </div>
---
+++ vue3-lsy-lotto/src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'LottoMainView',
+    component: () => import(/* webpackChunkName: "lottomainview" */ '../views/LottoMainView.vue')
+  }
+  // {
+  //   path: '/rsp',
+  //   name: 'RspMainView',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "rspmainview" */ '../views/RspMainView.vue')
+  // },
+  // {
+  //   path: '/job',
+  //   name: 'JobMainView',
+  //   component: () => import(/* webpackChunkName: "jobmainview" */ '../views/JobMainView.vue')
+  // },
+  // {
+  //   path: '/board',
+  //   name: 'BoardMainView',
+  //   component: () => import(/* webpackChunkName: "boardmainview" */ '../views/BoardMainView.vue')
+  // }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
